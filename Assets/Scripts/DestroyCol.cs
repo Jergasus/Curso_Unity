de93ab7813c7757fd4b22ecd8b94f6ee7bcfaa4d
Jergasus <jergasus2004@gmail.com>
2023-02-02 11:58:27 +0100

    Uploaded Game

--- Assets/Scripts/DestroyCol.cs
+++ Assets/Scripts/DestroyCol.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DestroyCol : MonoBehaviour
 {
+    public GameObject particle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,7 @@ public class DestroyCol : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
+            Instantiate(particle, this.transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
